65ed7c8b905b197fd21e01b9ef22d526a573f73b
ivorypaulina <ivorypaulina10@gmail.com>
2026-04-13 17:09:08 -0500

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        Libro libro1 = new Libro("ACOTAR", "Sarah J Maas", 2015);
-        Libro libro2 = new Libro("Warcross", "Marie Lu", 2014);
-
-        libro1.mostrarInformacion();
-        libro2.mostrarInformacion();
-    }
-}
